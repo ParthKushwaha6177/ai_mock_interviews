58f38dc37d926d543d804822171c271a909ac60c
ParthKushwaha6177 <145379306+ParthKushwaha6177@users.noreply.github.com>
2025-11-12 23:49:27 +0530

    Update route.ts

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -3,6 +3,17 @@ import { google } from "@ai-sdk/google";
 
 import { db } from "@/firebase/admin";
 import { getRandomInterviewCover } from "@/lib/utils";
+import admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_ADMIN_PROJECT_ID || '',
+      clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL || '',
+      privateKey: (process.env.FIREBASE_ADMIN_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+    }),
+  });
+}
 
 export async function POST(request: Request) {
   const { type, role, level, techstack, amount, userid } = await request.json();
