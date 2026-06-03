c9697f197e183c906de522020c4854f6421b38ab
ParthKushwaha6177 <145379306+ParthKushwaha6177@users.noreply.github.com>
2026-06-03 11:19:32 +0530

    done for good
    changes for better

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -33,7 +33,7 @@ export async function POST(request: Request) {
         ["Question 1", "Question 2", "Question 3"]
         
         Thank you! <3
-    `,
+      `,
     });
 
     const interview = {
@@ -53,7 +53,7 @@ export async function POST(request: Request) {
     return Response.json({ success: true }, { status: 200 });
   } catch (error) {
     console.error("Error:", error);
-    return Response.json({ success: false, error: error }, { status: 500 });
+    return Response.json({ success: false, error: String(error) }, { status: 500 });
   }
 }
 
